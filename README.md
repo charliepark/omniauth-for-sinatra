1aa9f792c1802e4d1b75aa2888b5fe09cad238a9
Charlie Park <charlie@pearbudget.com>
2011-04-11 14:07:58 -0400

    Added details to readme; Added actual code to app.rb

---
+++ README.md
@@ -0,0 +1,33 @@
+# Omniauth for Sinatra
+
+This is a template for launching Sinatra apps quickly and easily, using [OmniAuth](https://github.com/intridea/omniauth) as the authentication protocol. It differs from most of the "use Twitter with Sinatra" code bases, as it incorporates sessions and a User model in the database. This means your user should remain logged in as long as your server stays live.
+
+The instructions here assume you're using Twitter, but you can use any auth provider handled by OmniAuth.
+
+Most of the actual code here is simply a port/mashup of Ryan Bates's example from [RailsCasts #241: Simple OmniAuth](http://railscasts.com/episodes/241-simple-omniauth) and the OmniAuth readme.
+
+Once you have your auth codes, getting a new Sinatra app with authentication working is simply a matter of copying the app.rb file from here into a new directory, pasting in your auth codes, and then starting in on your app's code. It just works.
+
+
+## Getting Started Instructions
+
+### 1. Install Omniauth and the other required gems
+
+    gem install omniauth
+
+You'll also need to install data_mapper and dm-sqlite-adapter if you don't have them in your system.
+
+
+### 2. Get your Twitter Auth Keys
+
+At Twitter, [register for a new app](https://dev.twitter.com/apps/new). Note the Consumer Key and Consumer Secret strings it gives you.
+
+
+### 3. Enter your Auth Keys into the app.rb file
+
+The file app.rb has a place for your CONSUMER_KEY and CONSUMER_SECRET. Just replace them, and you're set. Fire up your local server ('ruby app.rb') and you can see your log-in setup.
+
+
+## A Minor Note
+
+Note that **you cannot use shotgun to test your app**, as you'll need sessions to persist in order to stay logged in, and shotgun restarts the server on every request. Starting your app with 'ruby app.rb' should work with no problems.
---
+++ app.rb
@@ -0,0 +1,57 @@
+%w(rubygems oa-oauth dm-core dm-sqlite-adapter dm-migrations sinatra).each { |dependency| require dependency }
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/database.db")
+
+class User
+  include DataMapper::Resource
+  property :id,         Serial
+  property :uid,        String
+  property :name,       String
+  property :nickname,   String
+  property :created_at, DateTime
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+# You'll need to customize the following line. Replace the CONSUMER_KEY and CONSUMER_SECRET with the values you got from Twitter (https://dev.twitter.com/apps/new).
+use OmniAuth::Strategies::Twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+
+enable :sessions
+
+helpers do
+  def current_user
+    @current_user ||= User.get(session[:user_id]) if session[:user_id]
+  end
+end
+
+get '/' do
+  if current_user
+    # the following line just tests to see that it's working. If you've logged in your first user, '/' should load: "1 ... 1"; You can then remove the following line, start using view templates, etc.
+    current_user.id.to_s + " ... " + session[:user_id].to_s 
+  else
+    '<a href="/sign_up">create an account</a> or <a href="/sign_in">sign in with Twitter</a>'
+    # if you replace the above line with the following line, the user gets signed in automatically. Could be useful. Could also break user expectations.
+    # redirect '/auth/twitter'
+  end
+end
+
+get '/auth/:name/callback' do
+  auth = request.env["omniauth.auth"]
+  user = User.first_or_create({ :uid => auth["uid"]}, { :uid => auth["uid"], :nickname => auth["user_info"]["nickname"], :name => auth["user_info"]["name"], :created_at => Time.now })
+  session[:user_id] = user.id
+  redirect '/'
+end
+
+["/sign_in/?", "/signin/?", "/log_in/?", "/login/?", "/sign_up/?", "/signup/?"].each do |path|
+  get path do
+    redirect '/auth/twitter'
+  end
+end
+
+["/sign_out/?", "/signout/?", "/log_out/?", "/logout/?"].each do |path|
+  get path do
+    session[:user_id] = nil
+    redirect '/'
+  end
+end
