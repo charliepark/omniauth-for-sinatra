9a7f8b66b93b2ada9ab46f801da46d32b38757b2
Charlie Park <charlie@pearbudget.com>
2011-04-11 23:41:00 -0400

    My regex's were overly ambitious. Reverting to an earlier (and wordier) version of route matching.

--- app.rb
+++ app.rb
@@ -44,12 +44,16 @@ get '/auth/:name/callback' do
 end
 
 # any of the following routes should work to sign the user in: /sign_up, /signup, /sign_in, /signin, /log_in, /login
-get %r{/[/log|sign_?up|in/]} do
-  redirect '/auth/twitter'
+["/sign_in/?", "/signin/?", "/log_in/?", "/login/?", "/sign_up/?", "/signup/?"].each do |path|
+  get path do
+    redirect '/auth/twitter'
+  end
 end
 
 # either /log_out, /logout, /sign_out, or /signout will end the session and log the user out
-get %r{/[/log|sign_?out/]} do
-  session[:user_id] = nil
-  redirect '/'
+["/sign_out/?", "/signout/?", "/log_out/?", "/logout/?"].each do |path|
+  get path do
+    session[:user_id] = nil
+    redirect '/'
+  end
 end
