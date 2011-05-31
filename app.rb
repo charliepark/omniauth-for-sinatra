abec8d13f58e7b531ef00f531ace3e2d50a46f29
Charlie Park <charlie@pearbudget.com>
2011-05-31 14:05:47 -0400

    Formatting tweaks, dropping below 80 chars per line.

--- app.rb
+++ app.rb
@@ -14,7 +14,9 @@ end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-# You'll need to customize the following line. Replace the CONSUMER_KEY and CONSUMER_SECRET with the values you got from Twitter (https://dev.twitter.com/apps/new).
+# You'll need to customize the following line. Replace the CONSUMER_KEY 
+#   and CONSUMER_SECRET with the values you got from Twitter 
+#   (https://dev.twitter.com/apps/new).
 use OmniAuth::Strategies::Twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 
 enable :sessions
@@ -27,23 +29,32 @@ end
 
 get '/' do
   if current_user
-    # the following line just tests to see that it's working. If you've logged in your first user, '/' should load: "1 ... 1"; You can then remove the following line, start using view templates, etc.
+    # The following line just tests to see that it's working.
+    #   If you've logged in your first user, '/' should load: "1 ... 1";
+    #   You can then remove the following line, start using view templates, etc.
     current_user.id.to_s + " ... " + session[:user_id].to_s 
   else
     '<a href="/sign_up">create an account</a> or <a href="/sign_in">sign in with Twitter</a>'
-    # if you replace the above line with the following line, the user gets signed in automatically. Could be useful. Could also break user expectations.
+    # if you replace the above line with the following line, 
+    #   the user gets signed in automatically. Could be useful. 
+    #   Could also break user expectations.
     # redirect '/auth/twitter'
   end
 end
 
 get '/auth/:name/callback' do
   auth = request.env["omniauth.auth"]
-  user = User.first_or_create({ :uid => auth["uid"]}, { :uid => auth["uid"], :nickname => auth["user_info"]["nickname"], :name => auth["user_info"]["name"], :created_at => Time.now })
+  user = User.first_or_create({ :uid => auth["uid"]}, { 
+    :uid => auth["uid"], 
+    :nickname => auth["user_info"]["nickname"], 
+    :name => auth["user_info"]["name"], 
+    :created_at => Time.now })
   session[:user_id] = user.id
   redirect '/'
 end
 
-# any of the following routes should work to sign the user in: /sign_up, /signup, /sign_in, /signin, /log_in, /login
+# any of the following routes should work to sign the user in: 
+#   /sign_up, /signup, /sign_in, /signin, /log_in, /login
 ["/sign_in/?", "/signin/?", "/log_in/?", "/login/?", "/sign_up/?", "/signup/?"].each do |path|
   get path do
     redirect '/auth/twitter'
